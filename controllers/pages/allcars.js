19b2389597cdb22a4e4a2cfd1fd43949565d88ad
Zeke Tinch <zeketinch@gmail.com>
2024-08-05 20:33:16 -0500

    added get all cars function

---
+++ controllers/pages/allcars.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { Car } = require('../../models');
+
+router.get('/', async (req, res) => {
+try {
+    const carData = await Car.findAll({
+        raw: true,
+        nest: true,
+    });
+
+    console.log(carData);
+
+    res.render('allCars', {
+        carData
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
+module.exports = router;
