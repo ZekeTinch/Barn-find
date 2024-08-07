53216c8befaf723a8114544591da6d6b989d89a0
Zeke Tinch <zeketinch@gmail.com>
2024-08-06 20:23:09 -0500

    added collectors page

---
+++ controllers/pages/collectors-page.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { User, Car } = require('../../models');
+
+router.get('/', async (req, res) => {
+try {
+    const collectorData = await User.findAll({
+        where: {
+            isPublic: true
+        },
+        include: [Car],
+        raw: true,
+        nest: true,
+    });
+
+    console.log(collectorData);
+
+    // find all users with public profiles
+    res.render('collector', {
+        collectorData
+    });
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const collectorData = await User.findByPk(req.params.id, {
+            include: [Car],
+            raw: true,
+            nest: true,
+        });
+
+        // if (!collectorData.isPublic) {
+        //     res.redirect('/');
+            // return;
+        // }
+    
+        console.log(collectorData);
+    
+        res.render('collector', {
+            collectorData
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
