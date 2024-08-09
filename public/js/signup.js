0aafc9480ef1ff941d6b003b8089a1595ed4e4a1
Zeke Tinch <zeketinch@gmail.com>
2024-08-08 20:55:41 -0500

    fixed signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,10 +1,10 @@
 const signupFormEl = document.querySelector('#signup-form');
 
 const showError = (parentEl, errorText) => {
-  const errorPEl = document.createElement('p');
-  errorPEl.classList.add('error-element');
-  errorPEl.textContent = errorText;
-  parentEl.appendChild(errorPEl);
+    const errorPEl = document.createElement('p');
+    errorPEl.classList.add('error-element');
+    errorPEl.textContent = errorText;
+    parentEl.appendChild(errorPEl);
 };
 
 const removeAllErrors = () => {
@@ -14,47 +14,52 @@ const removeAllErrors = () => {
 
 
 const signupFormHandler = async (event) => {
-  event.preventDefault();
-  removeAllErrors();
+    console.log('testing')
+    event.preventDefault();
+    removeAllErrors();
 
-  const first_name = document.querySelector('#first-name-signup').value.trim();
-  const last_name = document.querySelector('#last-name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+    console.log('hit');
 
-  const bodyObj = {
-    first_name,
-    last_name,
-    email,
-    password,
-  };
+    const first_name = document.querySelector('#first-name-signup').value.trim();
+    const last_name = document.querySelector('#last-name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
-  if (!first_name || !last_name || !email || !password) {
+    const bodyObj = {
+        first_name,
+        last_name,
+        email,
+        password,
+    };
+
     console.log(bodyObj);
-    showError(signupFormEl, 'Please fill out all fields.');
-    return;
-  }
 
-  try {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify(bodyObj),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (!response.ok) {
-      const res = await response.json();
-      console.log(res);
-      const errorMsg = res.errors[0].message;
-      showError(signupFormEl, errorMsg);
-      return;
+    if (!first_name || !last_name || !email || !password) {
+        console.log(bodyObj);
+        showError(signupFormEl, 'Please fill out all fields.');
+        return;
     }
 
-    document.location.replace('/dashboard');
-  } catch (err) {
-    console.log(err);
-    showError(signupFormEl, 'A signup error has ocurred.');
-  }
+    try {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify(bodyObj),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) {
+            const res = await response.json();
+            console.log(res);
+            const errorMsg = res.errors[0].message;
+            showError(signupFormEl, errorMsg);
+            return;
+        }
+
+        document.location.replace('/dashboard');
+    } catch (err) {
+        console.log(err);
+        showError(signupFormEl, 'A signup error has ocurred.');
+    }
 };
 
 
